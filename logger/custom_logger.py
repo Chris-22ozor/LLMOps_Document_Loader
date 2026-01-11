5293d55872e9e632537050d88579f4483a78c54f
unknown <chukoz71@gmail.com>
2026-01-11 00:17:15 -0500

    custom exception and custom logger added

--- logger/custom_logger.py
+++ logger/custom_logger.py
@@ -1,7 +1,7 @@
 import os 
 import logging 
 from datetime import datetime 
-import structlog
+# import structlog
 
 class CustomLogger:
     def __init__(self, log_dir="logs"):
@@ -12,49 +12,60 @@ class CustomLogger:
 
         #Timestamped log file (for persistence)
         log_file = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-        self.log_file_path = os.path.join(self.logs_dir, log_file)
+        log_file_path = os.path.join(self.logs_dir, log_file)
+
+         # configure logging
+        logging.basicConfig(
+            filename=log_file_path,
+            format="[ %(asctime)s ] %(levelname)s %(name)s (line:%(lineno)d) - %(message)s",
+            level = logging.INFO,
+        )
 
     
     def get_logger(self, name=__file__):
-        logger_name = os.path.basename(name)
-        ## name=__file__  captures current file name
+        return logging.getLogger(os.path.basename(name))
+        # logger_name = os.path.basename(name)
+        ## name=__file__  captures current file name which is custom_logger.py
 
-        #Configure logging for console + file (both JSON)
-        file_handler = logging.FileHandler(self.log_file_path)
-        file_handler.setLevel(logging.INFO)
-        file_handler.setFormatter(logging.Formatter("%(message)s")) #Raw JSON lines
+        # #Configure logging for console + file (both JSON)
+        # file_handler = logging.FileHandler(self.log_file_path)
+        # file_handler.setLevel(logging.INFO)
+        # file_handler.setFormatter(logging.Formatter("%(message)s")) #Raw JSON lines
 
 
-        console_handler = logging.StreamHandler()
-        console_handler.setLevel(logging.INFO)
-        console_handler.setFormatter(logging.Formatter("%(message)s"))
+        # console_handler = logging.StreamHandler()
+        # console_handler.setLevel(logging.INFO)
+        # console_handler.setFormatter(logging.Formatter("%(message)s"))
 
-        logging.basicConfig(
-            level=logging.INFO,
-            format="%(message)s", #Structlog will handle JSON rendering
-            handlers =[console_handler, file_handler]
-        )
+        # logging.basicConfig(
+        #     level=logging.INFO,
+        #     format="%(message)s", #Structlog will handle JSON rendering
+        #     handlers =[console_handler, file_handler]
+        # )
 
-        # Configure structlog for JSON structured logging
+        # # Configure structlog for JSON structured logging
 
-        structlog.configure(
-            processors=[
-                structlog.processors.TimeStamper(fmt="iso", utc=True, key="timestamp"),
-                structlog.processors.add_log_level,
-                structlog.processors.EventRenamer(to="event"),
-                structlog.processors.JSONRenderer()
-            ],
-            logger_factory= structlog.stdlib.LoggerFactory(),
-            cache_logger_on_first_use=True,
-        )
+        # structlog.configure(
+        #     processors=[
+        #         structlog.processors.TimeStamper(fmt="iso", utc=True, key="timestamp"),
+        #         structlog.processors.add_log_level,
+        #         structlog.processors.EventRenamer(to="event"),
+        #         structlog.processors.JSONRenderer()
+        #     ],
+        #     logger_factory= structlog.stdlib.LoggerFactory(),
+        #     cache_logger_on_first_use=True,
+        # )
 
-        return structlog.get_logger(logger_name)
+        # return structlog.get_logger(logger_name)
     
 # # ---Usage Example ---
-# if __name__ == "__main__":
-#         logger = CustomLogger().get_logger(__file__)
-#         logger.info("User uploaded a file", user_id=123, filename="report.pdf")
-#         logger.error("Failed to process PDF", error="File not found", user_id=123)
+if __name__ == "__main__":
+        logger = CustomLogger()
+        logger = logger.get_logger(__file__)
+        logger.info("Custom Logger initialized")
+        # logger = CustomLogger().get_logger(__file__)
+        # logger.info("User uploaded a file", user_id=123, filename="report.pdf")
+        # logger.error("Failed to process PDF", error="File not found", user_id=123)
 
 
     #
