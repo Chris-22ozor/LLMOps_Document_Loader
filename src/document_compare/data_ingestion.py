b082a3e655f30051427279bf27ca370909a92375
unknown <chukoz71@gmail.com>
2026-02-05 11:37:39 -0500

    document comparison code implemented end to end

--- src/document_compare/data_ingestion.py
+++ src/document_compare/data_ingestion.py
@@ -13,7 +13,7 @@ class DocumentIngestion:
     its more about file loading
     """
 
-    def __init__(self, base_dir):
+    def __init__(self, base_dir:str = "data\\document_compare"):
         self.log = CustomLogger().get_logger(__name__)
         self.base_dir = Path(base_dir) # this shows the path where we will get the data 
         self.base_dir.mkdir(parents=True, exist_ok =True) # this will create the directory if it doesn't exist
@@ -23,20 +23,48 @@ class DocumentIngestion:
     def delete_existing_files(self):
         """
         Delete existing files at the specific path
-
+        Meaning if already we have a file at the base dir, we will delete, else
+        we will upload and save the file. The next function(action) below will take care of that
         """
         try:
-            pass
+            if self.base_dir.exists() and self.base_dir.is_dir():
+                for file in self.base_dir.iterdir():
+                    if file.is_file():
+                        file.unlink()  # unlink means to delete the file
+                        self.log.info("File deleted", path=str(file))
+
+                self.log.info("Directory cleaned", directory=str(self.base_dir))
+
         except Exception as e:
             self.log.error(f"Error deleting existing files: {e}")
             raise DocumentPortalException ("Error occurred while deleting existing files", sys) 
 
-    def save_uploaded_files(self):
+    def save_uploaded_files(self, reference_file, actual_file):
         """
         Save uploaded files to a specific directory
+        reference_file is version 2 while actual file is version 1
         """
         try:
-            pass
+            self.delete_existing_files()
+            self.log.info("Existing files deleted successfully.")
+
+            ref_path = self.base_dir/reference_file.name
+            act_path = self.base_dir/actual_file.name
+
+            if not reference_file.name.endswith(".pdf") or not actual_file.name.endswith(".pdf"):
+                raise ValueError("Only PDF files are allowed")
+            
+            # loading the reference file and actual file
+            with open(ref_path, "wb") as f:
+                f.write(reference_file.getbuffer())
+
+            with open(act_path, "wb") as f:
+                f.write(actual_file.getbuffer())
+
+            self.log.info("Files saved", reference= str(ref_path), actual=str(act_path))
+            return ref_path, act_path
+
+
         except Exception as e:
             self.log.error(f"Error saving uploaded files: {e}")
             raise DocumentPortalException ("Error occurred while saving uploaded files", sys)
@@ -54,15 +82,31 @@ class DocumentIngestion:
             for page_num in range(doc.page_count):
                 doc.load_page(page_num)
                 text = page.get_text() # type: ignore
-                if text.strip():
-                    all_text.append(f"\n ----- Page {page_num + 1 }----- \n{text}")
-
+                if text.strip():  # if text hast white spaces, then strip it
+                    all_text.append(f"\n ----- Page {page_num + 1 }----- \n{text}") 
             self.log.info("PDF read successfully", file= str(pdf_path), pages =len(all_text))
 
-
-
-
+            return "\n".join(all_text)
 
         except Exception as e:
             self.log.error(f"Error reading PDF: {e}")
             raise DocumentPortalException ("Error reading PDF", sys)
+        
+    def combine_documents(self) -> str:
+        try:
+            content_dict = {}
+            doc_parts = []
+            for filename in sorted(self.base_dir.iterdir()):
+                if filename.is_file()  and filename.suffix ==".pdf":
+                    content_dict[filename.name] = self.read_pdf(filename)
+
+            for filename, content in content_dict.items():
+                doc_parts.append(f"Document: {filename}\n {content}")
+
+            combined_text = "\n\n".join(doc_parts)
+            self.log.info("Documents combined", count=len(doc_parts))
+            return combined_text
+
+        except Exception as e:
+            self.log.error(f"Error combining documents: {e}")
+            raise DocumentPortalException("An error occurred while combining documents", sys)
